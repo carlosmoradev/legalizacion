8f7169215e4f3ab1811ab98fe7e2220628667c79
Carlos Mario Mora <github@carlosmora.biz>
2015-06-16 15:36:29 -0500

    se comienzan a agregar eventos onClick desde las etiquetas a de los iconos, para enviar cada peticion al archivo php correspondiente en el directorio gen

--- legaliza.php
+++ legaliza.php
@@ -5,12 +5,12 @@
 	<title>Legalización de Documentos</title>
 	<link rel="stylesheet" href="static/styles/normalize.css">
 	<link rel="stylesheet" href="static/styles/design.css">
+	<script type="text/javascript" src="static/js/jquery-1.11.3.min.js"></script>
 	<script type="text/javascript" src="static/js/legaliza.js"></script>
 	<script type="text/javascript">
-	var opciones="left=100,top=100,width=250,height=150";
-	asignaVentana = window.open("","",opciones);
-	asignaVentana.moveTo(400,100);
-	
+	// var opciones="left=500,top=500,width=850,height=550";
+	// asignaVentana = window.open("","",opciones);
+	// asignaVentana.moveTo(400,100);
 	</script>
 <?php
 session_start();
@@ -28,7 +28,7 @@ include_once 'config/connection.php';
 
 // estructuro la tabla superior con el historico de los ultimos 3 documentos
 
-$ultimosTres="select * from documentos ORDER BY DOC_CREAC LIMIT 3";
+$ultimosTres="select DOC_TIPO,DOC_DESC,DOC_CREAC from documentos ORDER BY DOC_CREAC DESC LIMIT 3";
 $resultado=mysql_query($ultimosTres);
 
 echo "<table border='1'><tr><td>Tipo Documento</td><td>Descripcion Gasto</td><td>Fecha de creacion</td></tr>";
@@ -48,39 +48,39 @@ echo "</table>";
 <section id="ConsultaHistoria">
 <a href="#" id="consultaHistorial">Consulta historial</a>
 </section>
-<section  id="explicacionLegaliza" class="espacioIconosGastos">
+<!-- <section  id="explicacionLegaliza" class="espacioIconosGastos">
 	<h2>Procedimiento</h2>
 	<p>Seleccione el tipo de gasto que desea legalizar, 
 		luego la fecha del consumo y finalmente solicite la el formato de legalizacion correspondiente</p>
-</section>
+</section> -->
 
 <section id="gastoSeleccionado"></section>
 <!-- este espacio contiene el nombre seleccionado y la sigla -->
 <section id="seleccionIconos">
 
 <figure id="gastosReembolsables" class="espacioIconosGastos">
-	<a href="#" ><img src="/static/images/gastosReembolsables.png" class="iconoGastos"></a>
+	<a href="#" id="gReembolsa"><img src="/static/images/gastosReembolsables.png" class="iconoGastos"></a>
 	<figcaption>
 		Gastos Reembolsables
 	</figcaption>
 </figure>
 
 <figure id="gastosViaje" class="espacioIconosGastos">
-	<a href="#" ><img src="/static/images/gastosViaje.png"></a>
+	<a href="#" ><img src="/static/images/gastosViaje.png" class="iconoGastos"></a>
 	<figcaption>
 		Gastos de Viaje
 	</figcaption>
 </figure>
 
 <figure id="cajaMenor" class="espacioIconosGastos">
-	<a href="#" ><img src="/static/images/cajaMenor.png"></a>
+	<a href="#" ><img src="/static/images/cajaMenor.png" class="iconoGastos"></a>
 	<figcaption>
 		Ajuste Caja Menor
 	</figcaption>
 </figure>
 
 <figure id="tarjetaCredito" class="espacioIconosGastos">
-	<a href="#" ><img src="/static/images/tarjetaCredito.png"></a>
+	<a href="#" onclick="return confirmaTC();"><img src="/static/images/tarjetaCredito.png" class="iconoGastos"></a>
 	<figcaption>
 		Gastos con Tarjeta de Crédito
 	</figcaption>
@@ -96,7 +96,7 @@ echo "</table>";
 <!-- </section> -->
 
 <figure id="tarjetaDebito" class="espacioIconosGastos">
-	<a href="#" ><img src="/static/images/tarjetaDebito.png"></a>
+	<a href="#" ><img src="/static/images/tarjetaDebito.png" class="iconoGastos"></a>
 	<figcaption>
 		Gastos con Tarjeta Débito
 	</figcaption>
@@ -104,5 +104,6 @@ echo "</table>";
 </section>
 
 <section>termina</section>
+<a href="#" onclick="return pruebaOn();">click</a>
 <!-- Graba los cambios o los cancela -->
 </body>
