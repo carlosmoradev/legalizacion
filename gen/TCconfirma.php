a75818f34e6bdb94b6b9e2b4509179fe8d13e9eb
Carlos Mario Mora <github@carlosmora.biz>
2015-06-18 05:53:03 -0500

    intento realizar la conexion llamando al connection.php y con los errores de php habilitado muestra que debe modificarse a mysqli_connect.  Se retorna a la la conexion manual declarada directamente

--- gen/TCconfirma.php
+++ gen/TCconfirma.php
@@ -4,8 +4,13 @@
 	</head>
 <body>
 <?php
+ini_set('display_errors',1);
+error_reporting(E_ALL);
 $link = mysqli_connect("localhost","usulegal","dxPCAzmmmCVJcPbtBQ5EjJqd");
 mysqli_select_db($link,"legal");
+// session_start();
+// include_once '../config/connection.php';
+
 // $result = mysqli_query($link, "SELECT * FROM documentos WHERE DOC_SIGLA = 'TC' ORDER BY DOC_ID DESC LIMIT1");
 $result = mysqli_query($link, "SELECT * FROM documentos WHERE DOC_SIGLA = 'TC' ORDER BY DOC_ID DESC");
 mysqli_data_seek($result, 0);
