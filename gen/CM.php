e8b9f01547e9e6c67156efda4bd0f21f607f6d75
Carlos Mario Mora <github@carlosmora.biz>
2015-06-18 06:44:22 -0500

    en base a TC.php y TCconfirma.php creo los archivos CM.php y CMconfirma.php para realizar el registro y mostrar el resultado de Caja menor (CM)

--- gen/CM.php
+++ gen/CM.php
@@ -1,12 +1,19 @@
 <?php
 session_start();
-include_once 'config/connection.php';
+include_once '../config/connection.php';
 
 // $ultimosTres="select DOC_TIPO,DOC_DESC,DOC_CREAC from documentos ORDER BY DOC_CREAC LIMIT 3";
 // $resultado=mysql_query($ultimosTres);
 
-mysql_query("INSERT INTO documentos (DOC_TIPO,DOC_SIGLA,DOC_DESC) values ('T_CREDITO','TC','OTRA DESCRIPCION')");
+ mysql_query("INSERT INTO documentos (DOC_TIPO,DOC_SIGLA,DOC_DESC) values ('CAJA_MENOR','CM','GASTO DE CAJA MENOR')");
+
+	
+/* Redirect browser */
+header("Location: CMconfirma.php");
+ 
+/* Make sure that code below does not get executed when we redirect. */
+exit;
 
 $ultimo_id=mysql_insert_id();
-echo "Solicitud realizada $ultimo_id"
+echo "Formato Legalizacion Caja Menor No $ultimo_id"
 ?>
